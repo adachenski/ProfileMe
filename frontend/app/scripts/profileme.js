3088991a1bc61c6033dac7b46cf90f51f4fd09d3
nasko <adachenski@aol.com>
2017-01-07 16:33:43 -0800

    More CSS update: Second view fixed for mobile devices. Settings page flexbox update

--- frontend/app/scripts/profileme.js
+++ frontend/app/scripts/profileme.js
@@ -2,16 +2,14 @@
  * Created by Administrator on 11/6/2016.
  */
 
-// Checking if user has scrolled to change the height of the navbar
 $(document).ready(function () {
-    //$('.carousel').carousel({
-    //    interval: 3000
-    //});
+
     var $window = $(window);
 
     var hasScroled = false;
     $window.scroll(function () {
         var distance = $window.scrollTop()
+        // Checking if user has scrolled more than 5px to change the height of the navbar
         if (distance > 5 && hasScroled == false) {
             console.log(distance);
             $(".find-header").removeClass("initial-nav").addClass("changed-nav");
